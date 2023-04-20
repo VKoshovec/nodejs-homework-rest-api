39a1481a4af4e9dc9ff30348d4ef8c591a6ed2a9
VladimirKoshovec <koshavladmir@gmail.com>
2023-04-20 12:04:43 +0300

    fin_test

--- routes/api/auth-router.test.js
+++ routes/api/auth-router.test.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../../app');
-const controllers = require('../../controllers/authControllers');
 
 const { DB_HOST_TEST, PORT } = process.env;
 
-const {Users} = require('../../models/users');
-const { types, string, object } = require('joi');
-const { Types } = require('mongoose');
-
-describe("test signup (authControllers.registerUser)", ()=>{
+describe("test signup (auth-router.loginUser)", ()=>{
 
    let server = null; 
 
@@ -23,22 +18,36 @@ describe("test signup (authControllers.registerUser)", ()=>{
      await mongoose.disconnect();
    })
    
-   afterEach(async()=>{
-     await Users.deleteMany({});
-   })
-
-   test("test register route with correct data", async()=>{
+   it("test login route with correct data", async()=>{
     const sendData = {
-        email: "some@gmail.com",
+        email: "papa@gmail.com",
         password: "123456"
     };
 
-    const res = await request(app).post("/users/register").send(sendData);
-    expect(res.statusCode).toBe(201);
-    // expect(res.body).toContain("token");
+    const res = await request(app).post("/users/login").send(sendData);
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty('token');
     expect(res.body.user).toEqual({ email: sendData.email, subscription: "starter" });
     expect(typeof res.body.user.email).toBe("string");
     expect(typeof res.body.user.subscription).toBe("string");
+   });
+
+   it("test login route without data", async()=>{
+    const sendData = {};
+
+    const res = await request(app).post("/users/login").send(sendData);
+    expect(res.statusCode).toBe(400);
+   });
+
+
+   it("test login route with incorrect data", async()=>{
+    const sendData = {
+        email: "papa@gmail.co",
+        password: "12345"
+    };
+
+    const res = await request(app).post("/users/login").send(sendData);
+    expect(res.statusCode).toBe(401);
    });
 
 
